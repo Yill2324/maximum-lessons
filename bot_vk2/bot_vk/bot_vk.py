f172b03c2c266967ae5eb42e4dd906688e2e257c
yill <yill.cypher07@yande.ru>
2019-06-27 22:45:55 +0500

    бот весия 3

--- bot_vk2/bot_vk/bot_vk.py
+++ bot_vk2/bot_vk/bot_vk.py
@@ -3,6 +3,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id 
 from memes import get_memes
 from valute import get_course 
+from aniktod import get_ani
 
 def main():
     TOKEN = "c12be4a98e9a4b2621483784265704c9a8a6d387af43864143b3646e55e7c7b312dd4dbf412cca4d2ceb6"
@@ -17,6 +18,8 @@ def main():
     -мем - Я кину тебе мем.
     -Топ-Чарт - Ты получишь топовые песни.
     -Погода - Я расскажу тебе погоду в твоём городе.
+    -Плей-лист автора - Автор поделится своей музыкой.
+    -Любимая песня автора - Это нужно обьяснять???
     В дальнейшем бот будет дорабатываться <З
     """
 
@@ -28,7 +31,7 @@ def main():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             msg_text = event.text.lower()
 
-            if msg_text == "мемы":
+            if msg_text == "мем":
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(),attachment=get_memes ("-147286578")) 
             elif msg_text == "курс доллара":
                 valute_id = ["R01235"]
@@ -39,9 +42,16 @@ def main():
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message="https://music.yandex.ru/chart")
 
             elif msg_text == "погода":
-                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message="https://yandex.ru/pogoda")
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message="https://yandex.ru/pogoda") 
 
+            elif msg_text == "плей-лист автора":
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message="https://m.vk.com/away.php?to=https%3A%2F%2Fmusic.yandex.ru%2Fusers%2Fyill.cypher07%2Fplaylists%2F1013")
 
+            elif msg_text == "любимая песня автора":
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message="https://m.vk.com/away.php?to=https%3A%2F%2Fmusic.yandex.ru%2Falbum%2F6275186%2Ftrack%2F46516829") 
+
+            elif msg_text == "аникдот":
+                vk.messages.send(user_id=event.user_id, random_id=get_random_id(),attachment=get_ani ("-128400576")) 
 
             else: 
                 vk.messages.send(user_id=event.user_id, random_id=get_random_id(),message=hello ) 
